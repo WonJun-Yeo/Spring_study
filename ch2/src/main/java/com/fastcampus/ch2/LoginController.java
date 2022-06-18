57c4215d443e3c0a745e261f54129bfe7c13fb35
WonJun Yeo <rbgh004@gmail.com>
2022-06-18 14:37:31 +0900

    Theory and Practice on Session

--- ch2/src/main/java/com/fastcampus/ch2/LoginController.java
+++ ch2/src/main/java/com/fastcampus/ch2/LoginController.java
@@ -3,9 +3,12 @@ package com.fastcampus.ch2;
 import java.net.URLEncoder;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,8 +21,18 @@ public class LoginController {
 		return "loginForm";
 	}
 	
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		// 1. 세션을 종료
+		session.invalidate();
+		
+		// 2. 홈으로 이동
+		return "redirect:/";
+	}
+	
+	
 	@PostMapping("/login")
-	public String login(String id, String pwd, boolean rememberId, HttpServletResponse response) throws Exception {
+	public String login(@CookieValue("id") String cookieId, String id, String pwd, String toURL, boolean rememberId, HttpServletResponse response, HttpServletRequest request) throws Exception {
 		// 1. id와 pwd 확인
 		if (!loginCheck(id, pwd)) {
 			String msg = URLEncoder.encode("id 또는 pwd가 일치하지 않습니다.","utf-8");
@@ -29,6 +42,12 @@ public class LoginController {
 		}
 		
 		// 2-2. id와 pwd가 일치하면, 홈으로 이동
+			// 세션 객체 얻어오기
+		HttpSession session = request.getSession();
+		
+			// 세션 객체에 id를 저장
+		session.setAttribute("id", id);
+		
 			// 1. 쿠키 생성
 			// 2. 응답에 저장
 			// 3. 홈으로 이동
@@ -41,7 +60,10 @@ public class LoginController {
 			cookie.setMaxAge(0);
 			response.addCookie(cookie);
 		}
-;		return "redirect:/";
+		
+		toURL = toURL==null || toURL.equals("") ? "/" : toURL;
+		
+		return "redirect:/" + toURL;
 	}
 	
 	private boolean loginCheck(String id, String pwd) {
