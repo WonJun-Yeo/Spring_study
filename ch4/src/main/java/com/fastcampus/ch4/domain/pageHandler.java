a3f0f56d432a1bcb6af657af2c0e957a6495ca5b
WonJun Yeo <rbgh004@gmail.com>
2022-07-10 17:37:23 +0900

    Create DAO with Mybatis

---
+++ ch4/src/main/java/com/fastcampus/ch4/domain/pageHandler.java
@@ -0,0 +1,11 @@
+package com.fastcampus.ch4.domain;
+
+public class pageHandler {
+    int totalCnt;   // 총 게시물 갯수
+    int pageSize;   // 한 페이지의 크리
+    int navSize;    // 페이지 네비게이션의 크기
+    int totalPage;  // 전체 페이지의 수
+    int page;       // 현재 페이지
+    int beginPage;  // 네이게이션의 첫번째 페이지
+    int endPage;    // 네비게이션의 마지막 페이지
+}
