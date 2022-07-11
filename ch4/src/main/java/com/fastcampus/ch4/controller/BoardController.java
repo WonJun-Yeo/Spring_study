19af96ad7b859818cd81a9e28e53d554829ff3f8
WonJun Yeo <rbgh004@gmail.com>
2022-07-11 18:08:50 +0900

    Implement bulletin board read and remove functions

--- ch4/src/main/java/com/fastcampus/ch4/controller/BoardController.java
+++ ch4/src/main/java/com/fastcampus/ch4/controller/BoardController.java
@@ -3,11 +3,14 @@ package com.fastcampus.ch4.controller;
 import com.fastcampus.ch4.domain.BoardDto;
 import com.fastcampus.ch4.domain.PageHandler;
 import com.fastcampus.ch4.service.BoardService;
+import org.springframework.beans.Mergeable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -20,6 +23,44 @@ import java.util.Map;
 public class BoardController {
     @Autowired
     BoardService boardService;
+
+    @PostMapping("/remove")
+    public String remove(Integer bno, Integer page, Integer pageSize, Model m, HttpSession session, RedirectAttributes rattr) {
+        String writer = (String)session.getAttribute("id");
+
+        try {
+            m.addAttribute("page", page);
+            m.addAttribute("pageSize", pageSize);
+
+            int rowCnt = boardService.remove(bno, writer);
+
+            if (rowCnt != 1)
+                throw new Exception("board remove error");
+
+            // 세션에 저장하고, 넘겨받은 쪽에서 한 번 사용한뒤 세션에서 삭제된다.
+            rattr.addFlashAttribute("msg", "DEL_OK");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            rattr.addFlashAttribute("msg", "DEL_ERR");
+        }
+        return "redirect:/board/list";
+    }
+
+    @GetMapping("/read")
+    public String read(Integer bno, Integer page, Integer pageSize,Model m) {
+        try {
+            BoardDto boardDto = boardService.read(bno);
+
+            m.addAttribute(boardDto);
+            m.addAttribute("page", page);
+            m.addAttribute("pageSize", pageSize);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return "board";
+    }
 
     @GetMapping("/list")
     public String list(Integer page, Integer pageSize, Model m, HttpServletRequest request) {
@@ -40,6 +81,8 @@ public class BoardController {
             List<BoardDto> list = boardService.getPage(map);
             m.addAttribute("list", list);
             m.addAttribute("ph",pageHandler);
+            m.addAttribute("page", page);
+            m.addAttribute("pageSize", pageSize);
             boardService.getPage(map);
         } catch (Exception e) {
             throw new RuntimeException(e);
